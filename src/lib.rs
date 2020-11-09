0f8d55372b81856ec887dbfa399ab078d327a362
ryok-0319 <ryo.k.0319.tan2@gmail.com>
2020-11-09 16:37:59 +0900

    Add hello world

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,28 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+use log::info;
+use proxy_wasm::traits::*;
+use proxy_wasm::types::*;
+use std::time::Duration;
+
+#[no_mangle]
+pub fn _start() {
+    proxy_wasm::set_log_level(LogLevel::Trace);
+    proxy_wasm::set_root_context(|_| -> Box<dyn RootContext> {
+        Box::new(Scraper)
+    });
+}
+
+struct Scraper;
+
+impl Context for Scraper {}
+
+impl RootContext for Scraper {
+    fn on_vm_start(&mut self, _: usize) -> bool {
+        info!("Hello!");
+        self.set_tick_period(Duration::from_secs(5));
+        true
+    }
+
+    fn on_tick(&mut self) {
+        info!("World!!!");
     }
 }
